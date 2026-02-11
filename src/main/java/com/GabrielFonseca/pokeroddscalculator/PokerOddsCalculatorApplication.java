9144790570150e8a767b9cce266762cef9701cbc
GabrielFonseca02 <gabrielfonsecac02@gmail.com>
2026-02-11 14:23:10 -0300

    Primeiro commit - SpringBoot project structure

---
+++ src/main/java/com/GabrielFonseca/pokeroddscalculator/PokerOddsCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.GabrielFonseca.pokeroddscalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerOddsCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerOddsCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/GabrielFonseca/pokeroddscalculator/PokerOddsCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GabrielFonseca.pokeroddscalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerOddsCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
