8c6bcba50aa2d2fe1edd5f619b6b514b229f2b9e
GabrielFonseca02 <gabrielfonsecac02@gmail.com>
2026-03-11 20:11:38 -0300

    gera o baralho, gera cartas aleatorias para a mão consegue classificar a força da sua mao, com o fator de desempate. proximo passo: combinationGenerator. gerar 21 combinações diferentes para saber qual é de fato a melhor.

--- src/main/java/com/GabrielFonseca/pokeroddscalculator/service/HandEvaluator.java
+++ src/main/java/com/GabrielFonseca/pokeroddscalculator/service/HandEvaluator.java
@@ -3,6 +3,7 @@ package com.GabrielFonseca.pokeroddscalculator.service;
 import com.GabrielFonseca.pokeroddscalculator.model.*;
 
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -10,18 +11,19 @@ import java.util.stream.Collectors;
 
 public class HandEvaluator {
 
-    public HandResult evaluateFiveCards(List <Card> cards) {
 
 
-        if (cards.size() < 5) {
+    private HandResult evaluateFiveCards(List <Card> cards) {
+
+
+        if (cards.size() < 5 || cards.size() > 7) {
             throw new IllegalArgumentException("Hand must have at least 5 cards.");
         }
 
         //
         List<Integer> values = cards.stream()
-            .map(card -> card.getRank() //transforma a class Card em apenas um atributo, no caso getRank
-            .getValue())
-            .sorted((a,b)->a-b)
+            .map(c -> c.getRank().getValue()) //transforma a class Card em apenas um atributo, no caso getRank
+            .sorted(Collections.reverseOrder())
             .toList();
 
         boolean isFlush =   cards.stream().map(Card::getSuit).distinct().count() == 1;
@@ -32,48 +34,95 @@ public class HandEvaluator {
                 .toList(); // coloca tudo na lista "UniqueValues"
 
         boolean isStraight = false;
+        int straightHigh=0;
+
         if(uniqueValues.size()==5){
             int min = uniqueValues.get(0);
             int max = uniqueValues.get(4);
-            isStraight = (max - min == 4);// se lado direito for V isStraight tambem recebe V.
+             if(max - min == 4){// se lado direito for V isStraight tambem recebe V.
+                isStraight = true;
+                 straightHigh = max;
+            }
+
+             if(uniqueValues.equals(List.of(2,3,4,5,14))){
+                 isStraight = true;
+                 straightHigh = 5;
+             }
         }
 
-        Map<Rank, Long> frequency = cards.stream().map(Card::getRank).
-                collect(Collectors.groupingBy(rank->rank, Collectors.counting()));
+        Map<Integer, Long> frequency = values.stream()
+                .collect(Collectors.groupingBy(v -> v, Collectors.counting()));
+        //conta quantas vezes a chave apareceu na stream(fluxo)
 
-        List <Long> counts = frequency.values().stream().sorted(Collections.reverseOrder()).toList();
+        List<Map.Entry<Integer, Long>> sortedFrequency =
+                frequency.entrySet().stream() //entrySet: busca a chave e o valor sem precisar acessar 2x
+                        .sorted((a, b) -> {
+                            int freqCompare = b.getValue().compareTo(a.getValue());
+                            if (freqCompare == 0) {
+                                return b.getKey().compareTo(a.getKey());
+                            }
+                            return freqCompare;
+                        })
+                        .toList();
 
-        if(isFlush && isStraight){
-            return new HandResult(HandRank.StraightFlush,values);
-        }
-        if(counts.get(0)==4){
-            return new HandResult(HandRank.FourOfAKind,values);
-        }
-        if(counts.get(0)==3 && counts.get(1)==2){
-            return new HandResult(HandRank.FullHouse,values);
-        }
-        if(isFlush){
-            return new HandResult(HandRank.Flush,values);
-        }
-        if(isStraight){
-            return new HandResult(HandRank.Straight,values);
-        }
-        if(counts.get(0)==3){
-            return new HandResult(HandRank.ThreeOfAKind,values);
-        }
-        if(counts.get(0)==2 && counts.get(1)==2){
-            return new HandResult(HandRank.TwoPair,values);
-        }
-        if(counts.get(0)==2){
-            return new HandResult(HandRank.OnePair,values);
-        }
-        return new HandResult(HandRank.HighCard,values);
+        List<Integer> rankingValues = new ArrayList<>();
 
-        // Aqui depois vamos:
-        // 1. Gerar combinações de 5 cartas
-        // 2. Avaliar cada uma
-        // 3. Retornar a melhor
+        sortedFrequency.forEach(entry -> {
+            for (int i = 0; i < entry.getValue(); i++) {
+                rankingValues.add(entry.getKey());
+            }
+        });
 
+        // ===== CLASSIFICAÇÃO =====
 
+        if (isStraight && isFlush) {
+            if (straightHigh == 14) {
+                return new HandResult(HandRank.RoyalFlush, List.of(14));
+            }
+            return new HandResult(HandRank.StraightFlush, List.of(straightHigh));
+        }
+
+        if (sortedFrequency.get(0).getValue() == 4) {
+            return new HandResult(HandRank.FourOfAKind, rankingValues);
+        }
+
+        if (sortedFrequency.get(0).getValue() == 3 &&
+                sortedFrequency.get(1).getValue() == 2) {
+            return new HandResult(HandRank.FullHouse,
+                    List.of(sortedFrequency.get(0).getKey(),
+                            sortedFrequency.get(1).getKey()));
+        }
+
+        if (isFlush) {
+            return new HandResult(HandRank.Flush, values);
+        }
+
+        if (isStraight) {
+            return new HandResult(HandRank.Straight,
+                    List.of(straightHigh));
+        }
+
+        if (sortedFrequency.get(0).getValue() == 3) {
+            return new HandResult(HandRank.ThreeOfAKind, rankingValues);
+        }
+
+        if (sortedFrequency.get(0).getValue() == 2 &&
+                sortedFrequency.get(1).getValue() == 2) {
+
+            return new HandResult(HandRank.TwoPair,
+                    List.of(
+                            sortedFrequency.get(0).getKey(),
+                            sortedFrequency.get(1).getKey(),
+                            sortedFrequency.get(2).getKey()
+                    ));
+        }
+
+        if (sortedFrequency.get(0).getValue() == 2) {
+            return new HandResult(HandRank.OnePair, rankingValues);
+        }
+
+        return new HandResult(HandRank.HighCard, values);
     }
+
 }
+
---
+++ src/main/java/util/CombinationGenerator.java
@@ -0,0 +1,22 @@
+package util;
+import com.GabrielFonseca.pokeroddscalculator.model.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/*
+        objetivo é avaliar até 21 mãos da mesa
+        C(7,5) = 21 combinações
+     */
+
+public class CombinationGenerator {
+
+
+    /*public static List<List<Card>> generationCombinations(List<Card> cards) {
+
+    }*/
+
+}
