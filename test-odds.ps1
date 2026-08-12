05ba721ce206ce4a98ce1ead9a43a7a9413d49d2
GabrielFonseca02 <gabrielfonsecac02@gmail.com>
2026-08-12 18:44:55 -0300

    Corrige imagem do README e limpa arquivos de depuração
    - Substitui o docs/screenshot.png vazio pela captura real da interface
    - Renomeia HandoEvaluatorTest.java para HandEvaluatorTest.java
    - Remove request.json e test-odds.ps1 do versionamento e os adiciona
      ao .gitignore

--- test-odds.ps1
+++
@@ -1,5 +0,0 @@
-try {
-    Invoke-RestMethod -Uri "http://localhost:8080/api/odds" -Method Post -ContentType "application/json" -Body '{"heroCards":[{"rank":"ACE","suit":"HEARTS"}],"opponents":1,"simulations":10000}'
-} catch {
-    $_.ErrorDetails.Message
-}
